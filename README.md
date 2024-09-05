08013e539eeca8c87d25a56cca7198dd8bd44935
inhwa <jeoninhwa3@naver.com>
2024-09-05 17:38:09 +0900

    feat: 초기세팅 (패키지 설치)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Help-Me
-나만의 맞춤 식단 관리가 가능해요!
+# Health-Protect-Me
+
+<img src="./public/img_mockup.png"/>
+
+- [x] 나만을 위한 식단과 운동을 동시에 추천 받을 수 있어요!<br/>
+- [x] 커뮤니티 내에서 사용자들 간의 의견 공유가 가능해요!<br/><br/>
---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Health-Protect-Me",
+  description:
+    "헬스케어 전문가가 내 옆에!  나만을 위한 식단과 운동을 동시에 추천해주는 건강 관리",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
