b0668157db18c5f85a2f45426db1dcb5083414d1
inhwa <jeoninhwa3@naver.com>
2024-09-10 14:43:37 +0900

    fix: 오타 수정

--- src/app/(provider)/page.tsx
+++ src/app/(provider)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Mainpage = () => {
-  return <div>Mainpage</div>;
+const MainPage = () => {
+  return <div>MainPage</div>;
 };
 
-export default Mainpage;
+export default MainPage;
