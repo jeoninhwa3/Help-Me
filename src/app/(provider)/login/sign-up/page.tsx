cd41c4d206ffe8eb6c902d5b1626487ac6e90bbc
inhwa <jeoninhwa3@naver.com>
2024-09-13 11:13:25 +0900

    feat: 회원가입 기능 구현

--- src/app/(provider)/login/sign-up/page.tsx
+++ src/app/(provider)/login/sign-up/page.tsx
@@ -1,4 +1,33 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useState } from "react";
+
 const SignUpPage = () => {
+  const supabase = createClient();
+  const [email, setEmail] = useState<string>("");
+  const [nickname, setNickname] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          nickname,
+        },
+      },
+    });
+
+    if (error) {
+      console.error("Error during sign up:", error.message);
+    } else {
+      console.log("User signed up successfully:", data);
+    }
+  };
+
   return (
     <div className="absolute top-2/4 -translate-y-2/4 text-center">
       <p className="text-gray900 text-2xl font-extrabold">
@@ -7,7 +36,7 @@ const SignUpPage = () => {
       <p className="mt-1 text-gray600 text-base">
         나에게 꼭 맞는 식단을 추천해드려요!
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col mt-6">
           <label
             className="text-gray900 text-sm text-left"
@@ -20,6 +49,8 @@ const SignUpPage = () => {
             type="text"
             id="user-email"
             placeholder="abc@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="flex flex-col mt-6">
@@ -34,6 +65,8 @@ const SignUpPage = () => {
             type="text"
             id="user-nickname"
             placeholder="홍길동"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
           />
         </div>
         <div className="flex flex-col mt-6">
@@ -48,13 +81,14 @@ const SignUpPage = () => {
             type="text"
             id="user-nickname"
             placeholder="비밀번호 입력"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
         <button
           className="w-[320px] leading-5 mt-8 py-4 bg-pramary500 text-white border border-solid rounded-lg"
           type="submit"
-          disabled={true}
         >
           완료
         </button>
