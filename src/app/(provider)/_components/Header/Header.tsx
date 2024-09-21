9c00c130da9c9d341de516412938494ad0899885
inhwa <jeoninhwa3@naver.com>
2024-09-21 16:32:12 +0900

    feat: 헤더 드롭다운 기능 추가, 회원/비회원 시 아이콘 및 메뉴 설정

--- src/app/(provider)/_components/Header/Header.tsx
+++ src/app/(provider)/_components/Header/Header.tsx
@@ -7,7 +7,7 @@ import HeaderNav from "./_components/HeaderNav";
 
 const Header = () => {
   return (
-    <header className="fixed flex justify-between items-center w-full h-[56px] px-5 bg-white shadow-header-line">
+    <header className="fixed flex justify-between items-center w-full h-[56px] px-5 bg-white shadow-header-line z-10">
       <h1 className="py-4">
         <Link href="/">
           <Image src={logo} alt="HELPME" width={100} height={24} />
--- src/app/(provider)/_components/Header/_components/HeaderNav.tsx
+++ src/app/(provider)/_components/Header/_components/HeaderNav.tsx
@@ -1,38 +1,98 @@
 import Image from "next/image";
 import menu from "@/assets/icons/icon_hamburger.svg";
+import close from "@/assets/icons/icon_close.svg";
 import defaultProfileImg from "@/assets/images/img_default_profile.jpg";
 import { useUser } from "@/context/UserContext";
+import { useEffect, useRef, useState } from "react";
 
 const HeaderNav = () => {
   const { user, profileUrl } = useUser() || {};
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLUListElement | null>(null);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  const handleDropDown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(e.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav>
-      <button type="button" className="px-1 py-2">
-        <Image src={menu} alt="menu" width={24} height={24} />
+      <button
+        type="button"
+        className="px-1 py-2"
+        onClick={handleDropDown}
+        ref={buttonRef}
+      >
+        <Image
+          src={isDropdownOpen ? close : menu}
+          alt="menu"
+          width={24}
+          height={24}
+        />
       </button>
-      <ul className="absolute top-16 right-5 p-2 rounded-lg shadow-header-floating">
-        <li className="flex text-gray900">
-          <Image
-            className="rounded-full"
-            src={profileUrl ? profileUrl : defaultProfileImg}
-            alt="menu"
-            width={40}
-            height={40}
-          />
-          <p className="ml-2 text-gray900 text-sm">
-            반가워요! <br />
-            <strong className="mt-1 text-primary600 font-semibold">
-              {user?.nickname}
-            </strong>
-            님
-          </p>
-        </li>
-        {/* 링크태그 추가하기 */}
-        <li className="p-3 mt-1 text-gray900 text-sm">나만의 식단</li>
-        <li className="p-3 mt-1 text-gray900 text-sm">커뮤니티</li>
-        <li className="p-3 mt-1 text-gray900 text-sm">나의 프로필</li>
-        <li className="p-3 mt-1 text-gray900 text-sm">로그아웃</li>
-      </ul>
+      {user ? (
+        <>
+          {isDropdownOpen && (
+            <ul
+              ref={dropdownRef}
+              className="absolute top-16 right-5 p-2 rounded-lg bg-white shadow-header-floating z-10"
+            >
+              <li className="flex p-3 text-gray900">
+                <Image
+                  className="rounded-full"
+                  src={profileUrl ? profileUrl : defaultProfileImg}
+                  alt="menu"
+                  width={40}
+                  height={40}
+                />
+                <p className="ml-2 text-gray900 text-sm">
+                  반가워요! <br />
+                  <strong className="mt-1 text-primary600 font-semibold">
+                    {user?.nickname}
+                  </strong>
+                  님
+                </p>
+              </li>
+              {/* 링크태그 추가하기 */}
+              <li className="p-3 mt-1 text-gray900 text-sm">나만의 식단</li>
+              <li className="p-3 mt-1 text-gray900 text-sm">커뮤니티</li>
+              <li className="p-3 mt-1 text-gray900 text-sm">나의 프로필</li>
+              <li className="p-3 mt-1 text-gray900 text-sm">로그아웃</li>
+            </ul>
+          )}
+        </>
+      ) : (
+        <>
+          {isDropdownOpen && (
+            <ul
+              ref={dropdownRef}
+              className="absolute top-16 right-5 p-2 rounded-lg bg-white shadow-header-floating z-10"
+            >
+              {/* 링크태그 추가하기 */}
+              <li className="p-3 mt-1 text-gray900 text-sm">나만의 식단</li>
+              <li className="p-3 mt-1 text-gray900 text-sm">커뮤니티</li>
+              <li className="p-3 mt-1 text-gray900 text-sm">로그인</li>
+            </ul>
+          )}
+        </>
+      )}
     </nav>
   );
 };
