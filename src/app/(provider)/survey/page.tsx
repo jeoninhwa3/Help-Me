3b892293da209bc895a8b0fed9dbf230d54c2587
inhwa <jeoninhwa3@naver.com>
2024-10-11 10:19:24 +0900

    feat: 설문조사 완료 시 users 테이블 is_survey_done 값 변경

--- src/app/(provider)/survey/page.tsx
+++ src/app/(provider)/survey/page.tsx
@@ -58,24 +58,38 @@ const SurveyPage = () => {
       return console.log("로딩중");
     }
 
-    const { data: surveyData, error } = await supabase.from("survey").insert({
-      user_id: user.user_id,
-      height,
-      weight,
-      muscle,
-      body_fat: bodyFat,
-      year_of_birth: yearOfBirth,
-      exercise,
-      gender,
-      purpose,
-      allergies,
-    });
+    const { data: surveyData, insertError } = await supabase
+      .from("survey")
+      .insert({
+        user_id: user.user_id,
+        height,
+        weight,
+        muscle,
+        body_fat: bodyFat,
+        year_of_birth: yearOfBirth,
+        exercise,
+        gender,
+        purpose,
+        allergies,
+      });
 
-    if (error) {
-      console.log("설문조사 에러 =>", error);
+    if (insertError) {
+      console.log("설문조사 에러 =>", insertError);
     } else {
       console.log("설문조사 성공 =>", surveyData);
     }
+
+    const { data: surveyDoneData, error: surveyDoneError } = await supabase
+      .from("users")
+      .update({ is_survey_done: true })
+      .eq("user_id", user.user_id);
+
+    if (surveyDoneError) {
+      console.log("is_survey_done error", surveyDoneError);
+    } else {
+      console.log("is_survey_done 성공", surveyDoneData);
+    }
+
     router.push("/mydiet");
 
     fetchData();
